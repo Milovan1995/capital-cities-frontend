136b4c766c27296c2b1ad68f11091a6f6539131a
Milovan <milovan.antic314@gmail.com>
2023-12-10 17:19:03 +0100

    Adapt hero page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeroComponentComponent } from './hero-component/hero-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroComponentComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/hero-component/hero-component.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hero-component',
+  templateUrl: './hero-component.component.html',
+  styleUrl: './hero-component.component.css'
+})
+export class HeroComponentComponent {
+
+}
