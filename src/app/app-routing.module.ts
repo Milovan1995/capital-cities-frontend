370dfbd236ffb3f1e6e3d8fc56c709943c0e282c
Milovan <milovan.antic314@gmail.com>
2023-12-13 20:51:43 +0100

    Adapt routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HeroComponentComponent } from './hero-component/hero-component.component';
 import { TestComponent } from './test/test.component';
+import { AuthComponent } from './auth/auth.component';
 
 const routes: Routes = [
   { path: '', component: HeroComponentComponent },
   {
     path: 'test',
     component: TestComponent,
+  },
+  {
+    path: 'auth',
+    component: AuthComponent,
   },
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroComponentComponent } from './hero-component/hero-component.component';
 import { TestComponent } from './test/test.component';
+import { AuthComponent } from './auth/auth.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { LoginComponent } from './auth/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroComponentComponent,
-    TestComponent
+    TestComponent,
+    AuthComponent,
+    RegisterComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
