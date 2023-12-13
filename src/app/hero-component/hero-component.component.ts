3cc5658e427dda4cce0994f18ea4cb0231358f75
Milovan <milovan.antic314@gmail.com>
2023-12-13 13:43:35 +0100

    Adapt heroPage

--- src/app/hero-component/hero-component.component.ts
+++ src/app/hero-component/hero-component.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-hero-component',
   templateUrl: './hero-component.component.html',
-  styleUrl: './hero-component.component.css'
+  styleUrl: './hero-component.component.css',
 })
 export class HeroComponentComponent {
-
+  texts: string[] = [
+    'Guess the names of capital cities for random countries, collect scores,  track your progress and unlock achievements while competing with other players.',
+    'Very useful and fun game to play!',
+    "I didn't realise how many countries there are! Woah.",
+  ];
+  someText: string = this.texts[0];
 }
