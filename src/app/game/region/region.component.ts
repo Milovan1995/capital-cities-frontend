557fa05b916afb17e7ed556022c11e996d479d1f
Milovan <milovan.antic314@gmail.com>
2023-12-15 19:56:09 +0100

    Add region component

---
+++ src/app/game/region/region.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-region',
+  templateUrl: './region.component.html',
+  styleUrl: './region.component.css'
+})
+export class RegionComponent {
+
+}
