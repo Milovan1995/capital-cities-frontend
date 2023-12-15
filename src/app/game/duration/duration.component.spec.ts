f33cefac99ddc25270c7451be022e43165b2fb8e
Milovan <milovan.antic314@gmail.com>
2023-12-15 19:55:58 +0100

    Add duration component

---
+++ src/app/game/duration/duration.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { DurationComponent } from './duration.component';
+
+describe('DurationComponent', () => {
+  let component: DurationComponent;
+  let fixture: ComponentFixture<DurationComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [DurationComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(DurationComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/game/duration/duration.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-duration',
+  templateUrl: './duration.component.html',
+  styleUrl: './duration.component.css'
+})
+export class DurationComponent {
+
+}
