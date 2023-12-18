f6b791d099061c309b0c56895b7361c41fb6cd03
Milovan <milovan.antic314@gmail.com>
2023-12-18 10:00:04 +0100

    Adapt highscores

--- src/app/game/highscores/highscores.component.ts
+++ src/app/game/highscores/highscores.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-highscores',
   templateUrl: './highscores.component.html',
-  styleUrl: './highscores.component.css'
+  styleUrl: './highscores.component.css',
 })
 export class HighscoresComponent {
-
+  highscores: string[] = ['skor 1', 'skor2'];
 }
