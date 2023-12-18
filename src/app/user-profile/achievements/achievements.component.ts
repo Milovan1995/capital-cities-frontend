638365831baca2a5bb729b23c05265e2925e6161
Milovan <milovan.antic314@gmail.com>
2023-12-18 09:18:09 +0100

    Adapt achievements

--- src/app/user-profile/achievements/achievements.component.ts
+++ src/app/user-profile/achievements/achievements.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-achievements',
   templateUrl: './achievements.component.html',
-  styleUrl: './achievements.component.css'
+  styleUrl: './achievements.component.css',
 })
 export class AchievementsComponent {
-
+  achievements: string[] = ['Achievement 1', 'Achievement 2'];
 }
