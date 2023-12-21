5b8712344e0be7f68132646b12c41dbc91fce945
Milovan <milovan.antic314@gmail.com>
2023-12-21 09:34:33 +0100

    Add navbar where necessary

--- src/app/components/app/app.component.ts
+++ src/app/components/app/app.component.ts
@@ -1,8 +1,18 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {}
+export class AppComponent {
+  showNavbar: boolean = true;
+  constructor(private router: Router) {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.showNavbar = event.url !== '/';
+      }
+    });
+  }
+}
