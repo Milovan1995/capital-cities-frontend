c01d3e3ce150a9d9ea4a22872006ef75003b8ff1
Milovan <milovan.antic314@gmail.com>
2023-12-21 16:25:45 +0100

    Add routing from pages module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,43 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HeroComponentComponent } from './components/pages/hero-component/hero-component.component';
-import { TestComponent } from './test/test.component';
-import { LoginComponent } from './components/pages/login/login.component';
-import { UserProfileComponent } from './components/pages/user-profile/user-profile.component';
-import { GameComponent } from './components/pages/game/game.component';
-import { FeedbackComponent } from './components/pages/feedback/feedback.component';
-import { HighscoresComponent } from './components/pages/game/highscores/highscores.component';
-import { RegisterComponent } from './components/pages/register/register.component';
-
 const routes: Routes = [
-  { path: '', component: HeroComponentComponent },
   {
-    path: 'test',
-    component: TestComponent,
-  },
-  {
-    path: 'register',
-    component: RegisterComponent,
-  },
-  {
-    path: 'login',
-    component: LoginComponent,
-  },
-  {
-    path: 'user',
-    component: UserProfileComponent,
-  },
-  {
-    path: 'game',
-    component: GameComponent,
-  },
-  {
-    path: 'feedback',
-    component: FeedbackComponent,
-  },
-  {
-    path: 'highscores',
-    component: HighscoresComponent,
+    path: '',
+    loadChildren: () =>
+      import('./components/pages/pages.module').then((pm) => pm.PagesModule),
   },
 ];
 
--- src/app/components/pages/pages-routing.module.ts
+++ src/app/components/pages/pages-routing.module.ts
@@ -1,10 +1,49 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeroComponentComponent } from './hero-component/hero-component.component';
+import { TestComponent } from '../../test/test.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import { GameComponent } from './game/game.component';
+import { FeedbackComponent } from './feedback/feedback.component';
+import { HighscoresComponent } from './game/highscores/highscores.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', component: HeroComponentComponent },
+  {
+    path: 'test',
+    component: TestComponent,
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'user',
+    component: UserProfileComponent,
+  },
+  {
+    path: 'game',
+    component: GameComponent,
+  },
+  {
+    path: 'feedback',
+    component: FeedbackComponent,
+  },
+  {
+    path: 'highscores',
+    component: HighscoresComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class PagesRoutingModule { }
+export class PagesRoutingModule {}
