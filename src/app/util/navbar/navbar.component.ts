3cf1dc1335f8d3c6504b6c6bb759387ddd1a388f
Milovan <milovan.antic314@gmail.com>
2024-01-12 20:32:11 +0100

    Make navbar more reusable

--- src/app/util/navbar/navbar.component.ts
+++ src/app/util/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { Link } from '../../components/models/link';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -8,4 +9,13 @@ import { Link } from '../../components/models/link';
 })
 export class NavbarComponent {
   @Input() navbarItems: Link[];
+  constructor(private router: Router) {}
+  hasRoute(route: string): boolean {
+    return this.router.url === route;
+  }
+  partiallyContainsRoute(route: string): boolean {
+    return this.router.url.includes(route);
+  }
+  linkWhenLoggedIn: Link = new Link('Logout', '/home');
+  linkWhenLoggedOut: Link = new Link('Login', '/pages/login');
 }
