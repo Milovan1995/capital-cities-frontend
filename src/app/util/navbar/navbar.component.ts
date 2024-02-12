81d834b84a62e983eeec4f68f0e8dcaf77d4063f
Milovan <milovan.antic314@gmail.com>
2024-02-12 22:11:51 +0100

    Adapt navbar with isLoggedIn

--- src/app/util/navbar/navbar.component.ts
+++ src/app/util/navbar/navbar.component.ts
@@ -1,21 +1,30 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Link } from '../../components/models/link';
 import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css',
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
   @Input() navbarItems: Link[];
-  constructor(private router: Router) {}
+  isLoggedIn: boolean;
+  constructor(private router: Router, private authService: AuthService) {}
+  ngOnInit(): void {
+    this.isLoggedIn = this.authService.isLoggedIn();
+  }
   hasRoute(route: string): boolean {
     return this.router.url === route;
   }
   partiallyContainsRoute(route: string): boolean {
     return this.router.url.includes(route);
   }
-  linkWhenLoggedIn: Link = new Link('Logout', '/home');
-  linkWhenLoggedOut: Link = new Link('Login', '/pages/login');
+
+  logout() {
+    localStorage.clear();
+    this.ngOnInit();
+    window.location.href = '/home';
+  }
 }
