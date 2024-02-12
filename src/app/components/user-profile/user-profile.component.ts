9a242e765d11fe0b991295a6990c7103b98c5f5d
Milovan <milovan.antic314@gmail.com>
2024-02-12 22:23:19 +0100

    Adapt user page with actual username through service

--- src/app/components/user-profile/user-profile.component.ts
+++ src/app/components/user-profile/user-profile.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Link } from '../models/link';
+import { AuthService } from '../../services/auth.service';
 @Component({
   selector: 'app-user-profile',
   templateUrl: './user-profile.component.html',
   styleUrl: './user-profile.component.css',
 })
-export class UserProfileComponent {
-  username: string = 'Milovan';
+export class UserProfileComponent implements OnInit {
+  username: string;
+  constructor(private authService: AuthService) {}
+  ngOnInit(): void {
+    this.username = this.authService.getUserData().username;
+  }
   buttonRoute: Link = new Link('Play', '/pages/user/play');
   navbarLinks: Link[] = [
     new Link('Profile', '/pages/user'),
