0075876a1880d649801663ca8ebdb71a9bb48edf
Milovan <milovan.antic314@gmail.com>
2024-06-23 10:23:50 +0200

    Tweak capitals list

--- src/app/components/game/capitals-list/capitals-list.component.ts
+++ src/app/components/game/capitals-list/capitals-list.component.ts
@@ -1,42 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Link } from '../../models/link';
 import { CapitalService } from '../../../services/capital.service';
 import { Capital } from '../../models/capital';
-import { CapitalsResponse } from '../../models/capitalsResponse';
 import { CapitalCacheService } from '../../../services/capital-cache.service';
+import { Observable, catchError, defer, map, of } from 'rxjs';
 
 @Component({
   selector: 'app-capitals-list',
   templateUrl: './capitals-list.component.html',
   styleUrl: './capitals-list.component.css',
 })
-export class CapitalsListComponent implements OnInit {
+export class CapitalsListComponent {
   navbarLinks: Link[] = [
     new Link('About', '/about'),
     new Link('Play Now', '/capitals/play-game'),
   ];
-  capitals: Capital[] = [];
+  capitals$: Observable<Capital[]> = this.loadCapitals();
+  test$: Observable<Capital[]> = this.capitalService
+    .getAllCapitals()
+    .pipe(map((response) => response.capitals));
   errorMessage?: string;
 
   constructor(
     private capitalService: CapitalService,
     private capitalCacheService: CapitalCacheService
   ) {}
-
-  ngOnInit() {
-    this.capitals = this.capitalCacheService.getCapitalsFromCache();
-    if (!this.capitals) {
-      this.capitalService.getAllCapitals().subscribe({
-        next: (capitalsResponse: CapitalsResponse) => {
-          this.capitals = [...capitalsResponse.capitals];
-          this.capitalCacheService.setCapitalsInCache(this.capitals);
-        },
-        error: (error) => {
-          console.error('Error loading capitals:', error);
-          this.errorMessage =
-            'Apologies, our server might be undergoing a maintenance, please try exploring in a couple of minutes';
-        },
-      });
-    }
+  private loadCapitals(): Observable<Capital[]> {
+    return defer(() => {
+      const cachedCapitals = this.capitalCacheService.getCapitalsFromCache();
+      if (cachedCapitals && cachedCapitals.length > 0) {
+        return of(cachedCapitals);
+      } else {
+        return this.capitalService.getAllCapitals().pipe(
+          map((response) => {
+            this.capitalCacheService.setCapitalsInCache(response.capitals);
+            return response.capitals;
+          }),
+          catchError((error) => {
+            console.error(error);
+            this.errorMessage =
+              'Apologies, our server might be undergoing maintenance';
+            return of([]);
+          })
+        );
+      }
+    });
   }
 }
