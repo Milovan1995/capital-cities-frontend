95d6c82998e92b7b1067e807ba8ea54667d63c90
Milovan <milovan.antic314@gmail.com>
2024-08-23 23:37:13 +0200

    Remove server ts and tweak config

--- src/app/components/app/home/home.component.ts
+++ src/app/components/app/home/home.component.ts
@@ -1,15 +1,30 @@
-import { Component } from '@angular/core';
-import { Link } from '../../models/link';
+import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
-export class HomeComponent {
-  navbarItems: Link[] = [
-    new Link('About', '/about'),
-    new Link('Explore', '/capitals'),
-    new Link('Play Now', '/capitals/play-game'),
-  ];
+export class HomeComponent implements OnInit {
+  time$: BehaviorSubject<string> = new BehaviorSubject<string>(this.getTime());
+
+  interval: NodeJS.Timeout;
+
+  // TODO adapt css and add an image, use signals instead of observables
+  private getTime() {
+    const hours = new Date().getHours().toString().padStart(2, '0');
+    const minutes = new Date().getMinutes().toString().padStart(2, '0');
+    const seconds = new Date().getSeconds().toString().padStart(2, '0');
+    return `${hours}:${minutes}:${seconds}`;
+  }
+  ngOnInit(): void {
+    this.interval = setInterval(() => {
+      this.time$.next(this.getTime());
+    }, 1000);
+  }
+
+  ngOnDestroy(): void {
+    clearInterval(this.interval);
+  }
 }
