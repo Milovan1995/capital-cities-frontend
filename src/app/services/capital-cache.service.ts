d90af331ccd6112edb919f81e7ca2982c9704100
Milovan <milovan.antic314@gmail.com>
2024-08-23 21:09:53 +0200

    Refactor

--- src/app/services/capital-cache.service.ts
+++ src/app/services/capital-cache.service.ts
@@ -9,29 +9,36 @@ import {
 } from 'rxjs';
 import { Capital } from '../components/models/capital';
 import { CapitalService } from './capital.service';
+import { TranslateService } from '@ngx-translate/core';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CapitalCacheService {
   private capitals$: Observable<Capital[]>;
-  constructor(private capitalService: CapitalService) {
+  constructor(
+    private capitalService: CapitalService,
+    private _trans: TranslateService
+  ) {
     this.capitals$ = this.loadCapitals().pipe(shareReplay(1));
   }
+
   private loadCapitals(): Observable<Capital[]> {
     return defer(() => {
       return this.capitalService.getAllCapitals().pipe(
         map((response) => response.capitals),
         catchError((error) => {
-          console.error('Error caught in capital cache service', error);
+          console.error(this._trans.instant('error'), error);
           return throwError(() => error);
         })
       );
     });
   }
+
   getCapitals(): Observable<Capital[]> {
     return this.capitals$;
   }
+
   refreshCapitals(): void {
     this.capitals$ = this.loadCapitals().pipe(shareReplay(1));
   }
