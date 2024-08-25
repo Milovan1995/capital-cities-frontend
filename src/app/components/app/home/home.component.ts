847ffdcfab2c0115464e2a74f8cf1eebea678bab
Milovan <milovan.antic314@gmail.com>
2024-08-25 21:24:35 +0200

    Convert time on homepage into signal

--- src/app/components/app/home/home.component.ts
+++ src/app/components/app/home/home.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { Component, OnInit, signal } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -7,20 +6,15 @@ import { BehaviorSubject } from 'rxjs';
   styleUrl: './home.component.css',
 })
 export class HomeComponent implements OnInit {
-  time$: BehaviorSubject<string> = new BehaviorSubject<string>(this.getTime());
-
+  time$ = signal<string>(this.getTime());
   interval: NodeJS.Timeout;
 
-  // TODO adapt css and add an image, use signals instead of observables
   private getTime() {
-    const hours = new Date().getHours().toString().padStart(2, '0');
-    const minutes = new Date().getMinutes().toString().padStart(2, '0');
-    const seconds = new Date().getSeconds().toString().padStart(2, '0');
-    return `${hours}:${minutes}:${seconds}`;
+    return new Date().toLocaleTimeString('en-GB', { hour12: false });
   }
   ngOnInit(): void {
     this.interval = setInterval(() => {
-      this.time$.next(this.getTime());
+      this.time$.set(this.getTime());
     }, 1000);
   }
 
